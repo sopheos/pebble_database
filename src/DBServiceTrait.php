0491c49d25d9055dbcbc161d4bd3755601f526ac
mathieu <mathieu@sopheos.com>
2024-10-28 12:51:16 +0100

    Replace factory by DriverInterface

--- src/DBServiceTrait.php
+++ src/DBServiceTrait.php
@@ -7,23 +7,16 @@ use InvalidArgumentException;
 trait DBServiceTrait
 {
     private ?DriverInterface $db = null;
-    private $dbFactory = null;
 
-    public function registerDbFactory(callable $dbFactory)
+    public function registerDbFactory(DriverInterface $driver)
     {
-        $this->dbFactory = $dbFactory;
+        $this->db = $driver;
     }
 
     public function db(): DriverInterface
     {
         if ($this->db === null) {
-            $factory = $this->dbFactory;
-            $db = $factory && is_callable($factory) ? $factory() : null;
-            if (! ($db instanceof DriverInterface)) {
-                throw new InvalidArgumentException("dbFactory does not implements " . DriverInterface::class);
-            }
-
-            $this->db = $db;
+            throw new InvalidArgumentException(DriverInterface::class . " not registered");
         }
 
         return $this->db;
